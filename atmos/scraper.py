a17572bb28bc081c6bd7cc5d11cee3d455e18c57
Clement <clementlee1999@gmail.com>
2023-06-18 14:14:40 +0800

    overwrite from local version, added venv, requirements.txt

--- atmos/scraper.py
+++ atmos/scraper.py
@@ -5,102 +5,96 @@ import asyncio
 import json
 import os
 
-
 @dataclass
 class Product:
-  gender: str
-  name: str
-  price: str
-  sku_id: str
-  colourway: str
-  raffle_status: str
-  raffle_end_date: str
-  raffle_url: str
+    gender: str
+    name: str
+    price: str
+    sku_id: str
+    colourway: str
+    raffle_status: str
+    raffle_end_date: str
+    raffle_url: str
 
-
-def get_product_info_from_raffle_page(session):
-  res = session.get("https://raffle.atmos-kl.com/")
-  products = res.html.find("section#products-section",
-                           first=True).find("article")
-
-  product_list = []
-  for product in products:
-    product_info = product.text.split("\n")
-    gender = product_info[1]
-    name = product_info[2]
-    price = product_info[3]
-    raffle_status = product_info[4]
-    raffle_end_date = product_info[0]
-    raffle_url = product.find("a", first=True).attrs["href"]
-    product_list.append(
-      Product(gender=gender,
-              name=name,
-              price=price,
-              sku_id=None,
-              colourway=None,
-              raffle_status=raffle_status,
-              raffle_end_date=raffle_end_date,
-              raffle_url=raffle_url))
-
-  json_first_product_raffle_url_dict = None
-  try:
-    with open('data.json', 'r') as file:
-      json_first_product_raffle_url_dict = json.load(file)
-  except Exception as e:
-    pass
-
-  scraped_first_product = product_list[0]
-  scraped_first_product_dict = asdict(scraped_first_product)
-  scraped_first_product_raffle_url_dict = dict(
-    map(lambda key: (key, scraped_first_product_dict[key]), ["raffle_url"]))
-
-  if json_first_product_raffle_url_dict == None:
-    with open('data.json', 'w') as file:
-      json.dump(scraped_first_product_raffle_url_dict, file)
-  else:
-    if json_first_product_raffle_url_dict == scraped_first_product_raffle_url_dict:
-      return None
+def run_replit_config():
+    config = ConfigParser()
+    config.read("config.ini")
+    name = config["replit_atmos"]["name"]
+    path = config["replit_atmos"]["path"]
+    os.chdir(f"{path}/{name}")
+                
+def get_product_info_from_raffle_page():
+    session = HTMLSession()
+    res = session.get("https://raffle.atmos-kl.com/")    
+    products = res.html.find("section#products-section", first = True).find("article")
+        
+    product_list = []
+    for product in products:
+        product_info = product.text.split("\n")
+        gender = product_info[1]
+        name = product_info[2]
+        price = product_info[3]
+        raffle_status = product_info[4]
+        raffle_end_date = product_info[0]
+        raffle_url = product.find("a", first=True).attrs["href"]
+        product_list.append(Product(gender = gender, 
+                                    name = name, 
+                                    price = price,
+                                    sku_id = None, 
+                                    colourway = None, 
+                                    raffle_status = raffle_status, 
+                                    raffle_end_date = raffle_end_date, 
+                                    raffle_url = raffle_url))
+    
+    json_first_product_raffle_url_dict = None
+    try:
+        with open('data.json', 'r') as file:    
+            json_first_product_raffle_url_dict = json.load(file)
+    except Exception as e:
+        pass
+    
+    scraped_first_product = product_list[0]
+    scraped_first_product_dict = asdict(scraped_first_product)
+    scraped_first_product_raffle_url_dict = dict(map(lambda key: (key, scraped_first_product_dict[key]), ["raffle_url"]))
+    
+    if json_first_product_raffle_url_dict == None:
+        with open('data.json', 'w') as file:
+            json.dump(scraped_first_product_raffle_url_dict, file)
     else:
-      with open('data.json', 'w') as file:
-        json.dump(scraped_first_product_raffle_url_dict, file)
-
-  return product_list
-
+        if json_first_product_raffle_url_dict == scraped_first_product_raffle_url_dict:
+            return None
+        else:
+            with open('data.json', 'w') as file:
+                json.dump(scraped_first_product_raffle_url_dict, file)
+                      
+    return product_list
 
 async def get_product_info_from_pdp(a_session, url):
-  res = await a_session.get(url)
-  product_desc = res.html.find("div#main-product-description",
-                               first=True).find("li")
-  sku_id = product_desc[0].text.split(":")[-1].strip()
-  colourway = product_desc[1].text.split(":")[-1].strip()
-  return (url, sku_id, colourway)
-
-
+    res = await a_session.get(url)
+    product_desc = res.html.find("div#main-product-description", first = True).find("li")
+    sku_id = product_desc[0].text.split(":")[-1].strip()
+    colourway = product_desc[1].text.split(":")[-1].strip()
+    return (url, sku_id, colourway)
+    
 async def main(product_url_list):
-  a_session = AsyncHTMLSession()
-  tasks = (get_product_info_from_pdp(a_session, url)
-           for url in product_url_list)
-  return await asyncio.gather(*tasks)
-
-
+    a_session = AsyncHTMLSession()    
+    tasks = (get_product_info_from_pdp(a_session, url) for url in product_url_list) 
+    return await asyncio.gather(*tasks)
+    
 if __name__ == "__main__":
+        
+    # run_replit_config()
 
-  config = ConfigParser()
-  config.read("config.ini")
-
-  os.chdir(fr'{config["atmos_replit"]["path"]}/atmos')
-
-  session = HTMLSession()
-  product_list = get_product_info_from_raffle_page(session)
-
-  if product_list != None:
-    product_url_list = list(map(lambda x: x.raffle_url, product_list))
-    product_desc_list = asyncio.run(main(product_url_list))
-
-    for product in product_list:
-      for desc in product_desc_list:
-        if product.raffle_url == desc[0]:
-          product.sku_id = desc[1]
-          product.colourway = desc[2]
-
-  print(product_list)
+    product_list = get_product_info_from_raffle_page()
+ 
+    if product_list != None:    
+        product_url_list = list(map(lambda x: x.raffle_url, product_list))
+        product_desc_list = asyncio.run(main(product_url_list))
+        
+        for product in product_list:
+            for desc in product_desc_list:
+                if product.raffle_url == desc[0]: 
+                    product.sku_id = desc[1]
+                    product.colourway = desc[2]
+                
+    print(product_list)
