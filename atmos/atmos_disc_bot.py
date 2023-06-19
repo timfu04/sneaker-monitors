5b13ce13ecc74ce988bf39bce3703a094f626f9b
Clement <clementlee1999@gmail.com>
2023-06-19 15:18:18 +0800

    added comments

--- atmos/atmos_disc_bot.py
+++ atmos/atmos_disc_bot.py
@@ -1,10 +1,10 @@
 import os
 import discord
-import asyncio
 import datetime
 from dotenv import load_dotenv
 from discord.ext import tasks
-from atmos_scraper import run_replit_config, get_product_info_from_raffle_page, start_async_pdp_scrape
+from typing import List
+from atmos_scraper import Product, run_replit_config, get_product_info_from_raffle_page, start_async_pdp_scrape
 
 load_dotenv("atmos/.env")
 
@@ -17,20 +17,25 @@ def run_discord_bot():
     async def on_ready():
         scrape_data.start()
     
-    @tasks.loop(minutes=1)
+    @tasks.loop(minutes=30) # Repeat task every 30 minutes
     async def scrape_data():
         product_list = get_product_info_from_raffle_page() 
         if product_list != None:    
             product_url_list = list(map(lambda x: x.raffle_url, product_list))
-            product_desc_list = await start_async_pdp_scrape(product_url_list)
+            product_desc_list = await start_async_pdp_scrape(product_url_list)    
             for product in product_list:
                 for desc in product_desc_list:
                     if product.raffle_url == desc[0]: 
                         product.sku_id = desc[1]
                         product.colourway = desc[2]
             await send_embed(product_list)
-                    
-    async def send_embed(product_list):
+                               
+    async def send_embed(product_list: List[Product]):
+        """ Send embeds based on list of scraped products
+
+        Args:
+            product_list (List[Product]): List of scraped products
+        """
         channel_id = int(os.getenv("CHANNEL_ID"))
         channel = client.get_channel(channel_id)
         if channel:   
@@ -52,7 +57,6 @@ def run_discord_bot():
                 embed.add_field(name = "\u200B", value = "\u200B") # New line
                 embed.set_footer(text = "Atmos KL", icon_url = "https://atmos-kl.com/pub/media/favicon/stores/1/atmos-favicon-01.jpg")
                 await channel.send(embed = embed)
-                await asyncio.sleep(1)
                     
     @scrape_data.before_loop
     async def before_scrape_data():
--- atmos/atmos_scraper.py
+++ atmos/atmos_scraper.py
@@ -1,6 +1,7 @@
 from requests_html import HTMLSession, AsyncHTMLSession
 from dataclasses import dataclass, asdict
 from configparser import ConfigParser
+from typing import Union, List, Tuple
 import asyncio
 import json
 import os
@@ -16,20 +17,37 @@ class Product:
     raffle_end_date: str
     raffle_url: str
     img_url: str
-    
-def find_missing_products(scraped_dict_list, json_dict_list):
-    json_raffle_urls = [product.raffle_url for product in json_dict_list]
-    missing_products = [product for product in scraped_dict_list if product.raffle_url not in json_raffle_urls]
+                
+def find_missing_products(scraped_product_list: List[Product], json_product_list: List[Product]) -> List[Product]:
+    """ Find missing products that are in "scraped_dict_list" but not in "json_dict_list"
+
+    Args:
+        scraped_product_list (List[Product]): List of scraped products
+        json_product_list (List[Product]): List of products from JSON
+
+    Returns:
+        List[Product]: Missing products that are in "scraped_dict_list" but not in "json_dict_list"
+    """
+    json_raffle_urls = [product.raffle_url for product in json_product_list]
+    missing_products = [product for product in scraped_product_list if product.raffle_url not in json_raffle_urls]
     return missing_products
 
-def run_replit_config():
+def run_replit_config() -> None:
+    """ Get and change to script directory in Replit environment
+    """
     config = ConfigParser()
     config.read("config.ini")
     name = config["replit_atmos"]["name"]
     path = config["replit_atmos"]["path"]
     os.chdir(f"{path}/{name}")
                 
-def get_product_info_from_raffle_page():
+def get_product_info_from_raffle_page() -> Union[List, None]:
+    """ Scrape data from raffle page, process data scraped, save or update data into JSON if needed, find different between scraped data and JSON data
+
+    Returns:
+        Union[List, None]: Returns list when JSON data is empty or when scraped data and JSON data not same, 
+        returns None when scraped data and JSON data is the same
+    """
     session = HTMLSession()
     res = session.get("https://raffle.atmos-kl.com/")    
     products = res.html.find("section#products-section", first = True).find("article")
@@ -76,14 +94,32 @@ def get_product_info_from_raffle_page():
                 json.dump(scraped_product_dict_list, file)
             return find_missing_products(product_list, json_product_list)
         
-async def get_product_info_from_pdp(a_session, url):
+async def get_product_info_from_pdp(a_session: AsyncHTMLSession, url: str) -> Tuple[str, str, str]:
+    """ Extract SKU id and colourway from product detail page
+
+    Args:
+        a_session (AsyncHTMLSession): Async HTML session
+        url (str): Product detail page URL
+
+    Returns:
+        Tuple[str, str, str]: Tuple contains URL, SKU id, colourway
+    """
     res = await a_session.get(url)
     product_desc = res.html.find("div#main-product-description", first = True).find("li")
     sku_id = product_desc[0].text.split(":")[-1].strip()
     colourway = product_desc[1].text.split(":")[-1].strip()
     return (url, sku_id, colourway)
     
-async def start_async_pdp_scrape(product_url_list):
+async def start_async_pdp_scrape(product_url_list: list) -> List[Tuple]:
+    """ Define a generator of coroutines of "(get_product_info_from_pdp(a_session, url)" for each URL,
+        gather and await all coroutines executed
+
+    Args:
+        product_url_list (list): List of product URLs
+
+    Returns:
+        List[Tuple]: List of tuples, each containing URL, SKU id, colourway
+    """
     a_session = AsyncHTMLSession()    
     tasks = (get_product_info_from_pdp(a_session, url) for url in product_url_list) 
     return await asyncio.gather(*tasks)
